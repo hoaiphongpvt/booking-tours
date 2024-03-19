6a34e4c42c3d140e9c3f9a28508cfccdfa7b36ab
hoaiphongpvt <hoaiphongpvt@gmail.com>
2024-03-20 01:17:17 +0700

    Update Checkout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -79,8 +79,10 @@ exports.webhookCheckout = (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
+
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
