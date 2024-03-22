e75692fa57bdb75c7f0043cbb8c4705294711a62
hoaiphongpvt <hoaiphongpvt@gmail.com>
2024-03-22 15:21:33 +0700

    Updated Sign Up

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,7 @@ export const signup = async (name, email, password, passwordConfirm) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:3000/api/v1/users/signup',
+      url: '/api/v1/users/signup',
       data: {
         name,
         email,
